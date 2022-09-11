5320680bd30fc73c76b15501265bcbda8ea262c1
Daniela Romero <darogu17@outlook.com>
2022-09-11 15:09:03 -0500

    estructuracion de las vistas

---
+++ BeautyInternational.App/BeautyInternational.App.Frontend/Pages/ModUsuario/Modificacion.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BeautyInternational.App.Fronted.Pages
+{
+    public class ModificacionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
